2571d003f3f54ace29f03e3a09a34c7b0f840155
soorajtk3 <97935722+soorajtk3@users.noreply.github.com>
2022-01-24 18:38:41 +0530

    final solution

---
+++ task.py
@@ -0,0 +1,17 @@
+#first problem solution
+
+def sentence(text):
+    return len(text.split(".")) 
+    
+text = "i am a programmer. what do i do? i program. which language do i program in? why, Python of cousrse!"
+output = sentence(text)
+print(output)      
+
+
+#second problem solution
+
+def text(data):
+    result = len(data.split())
+    return result
+out = text("hello sooraj how are you?")
+print(out)    
